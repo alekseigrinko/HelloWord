c3748bd0326228645751fee537595efb0d1a3c0a
Aleksei Grinko <als.grinko@yandex.ru>
2022-12-17 17:00:49 +0400

    ifelse

---
+++ example004_ifelse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите имя пользователя!");
+string userName = Console.ReadLine();
+if (userName == "Маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.WriteLine("Привет " + userName);
+}
